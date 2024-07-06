7a95c2ded96682274e9753e0ad456a1240367e7c
Stefanos Ugbit <stefanosu422@gmail.com>
2024-07-05 23:17:41 -0400

    changed filenmaes

--- LibraryManagementSystem/Models/Book.cs
+++ LibraryManagementSystem/Models/Book.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 namespace LibraryManagementSystem.Models
 {
-	public class BookService
-	{
-		public BookService()
-		{
-
-        //fields for book info
+    public class Book
+    {
+         //fields for book info
         public int Id { get; set; }
         public required string Title { get; set; }
         public required string Author { get; set; }
         public int Year { get; set; }
+        
     }
-	}
+
 }
+
 
