1ef2abe8886a6a47dfce7a53330dd3577a95ae5d
Stefanos Ugbit <stefanosu422@gmail.com>
2024-07-09 20:04:25 -0400

    updated booksController added books

--- LibraryManagementSystem/Controllers/BookController.cs
+++ LibraryManagementSystem/Controllers/BookController.cs
@@ -3,19 +3,19 @@ using LibraryManagementSystem.Models;
 
 namespace LibraryManagementSystem.Controllers
 {
-	public class BookController
-	{
-		//create methods here for CRUD operations on books
+    public class BookController
+    {
+        //create methods here for CRUD operations on books
 
-		//creating a new instance of a Book to then add.
-		BookController bookController = new BookController();
+        //creating a new instance of a Book to then add.
+        BookController bookController = new BookController();
 
         //adding new book obj with parameters of the book
 
 
 
-		public void AddBook(Book book)
-		{
+        public void AddBook(Book book)
+        {
             //this method will add a Book to the list of Books.
 
 
@@ -26,33 +26,57 @@ namespace LibraryManagementSystem.Controllers
 
         }
 
+        private List<Book> books = new List<Book>
+    {
+        new Book { Id = 1, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", Price = 10.99m },
+        new Book { Id = 2, Title = "1984", Author = "George Orwell", Price = 8.99m },
+        new Book { Id = 3, Title = "To Kill a Mockingbird", Author = "Harper Lee", Price = 9.99m }
+    };
+
 
         public void GetAllBooks(Book book)
-		{
+        {
             //This method will Get a list of books
             // Get all books
-            var books = bookController.GetAllBooks();
+             IEnumerable<Book> GetAllBooks()
+            {
+                return books;
+            }
+
+        }
+
+        public void UpdateBook(Book book)
+        {
+            //this method will update a book
+        }
+
+
+        public void RemoveBook(Book book)
+        {
+            //this method will remove a book from the list of books.
+        }
+
+
+        public class Program
+        {
+            public static void Main()
+            {
+                var bookController = new BookController();
+                var books = bookController.GetAllBooks();
+
+                foreach (var book in books)
+                {
+                    Console.WriteLine($"Id: {book}, Title: {book.Title}, Author: {book.Author}, Price: {book.Price}");
+                }
+            }
+
+
         }
 
         private IEnumerable<object> GetAllBooks()
         {
             throw new NotImplementedException();
         }
-
-        public void UpdateBook(Book book)
-		{
-			//this method will update a book
-		}
-
-
-		public void RemoveBook(Book book)
-		{
-			//this method will remove a book from the list of books.
-		}
-
-
-	}
-
-
+    }
 }
 
