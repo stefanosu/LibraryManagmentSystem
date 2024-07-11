6cc6cc64807ce3af242d75f3cfecc0595d3321ca
Stefanos Ugbit <stefanosu422@gmail.com>
2024-07-10 22:03:22 -0400

    moved main method into program.cs file from bookController

--- LibraryManagementSystem/Controllers/BookController.cs
+++ LibraryManagementSystem/Controllers/BookController.cs
@@ -50,39 +50,6 @@ namespace LibraryManagementSystem.Controllers
                 books.Remove(book);
             }
         }
-
-
-        public class Program
-        {
-            public static void Main()
-            {
-                //instantiating a new instance of BookController here
-                BookController bookController = new BookController();
-
-                //Adding a new book
-                bookController.AddBook(new Book { Id = 1, Title = "I know why the cage bird sings", Author = "Maya Angelou", Price = 20.00m, Year = 1975 });
-
-                //get all books
-                var allBooks = bookController.GetAllBooks();
-                foreach(var book in allBooks)
-                {
-                    Console.WriteLine($"Id: {book.Id}, Title: {book.Title}, Author: {book.Author}, Price: {book.Price}, Year: {book.Year}");
-                }
-
-                //update book
-                bookController.UpdateBook(2, new Book { Id = 2, Title = "Ishamel" ,Author = "Daniel Quinn", Price = 25m , Year = 1995 });
-
-                //remove book
-                bookController.RemoveBook(3);
-
-                //get allBooks after update and remove
-                allBooks = bookController.GetAllBooks();
-                foreach(var book in allBooks)
-                {
-                    Console.WriteLine($"Id: {book.Id}, Title: {book.Title}, Author: {book.Author}, Price: {book.Price}, Year: {book.Year}");
-                }
-            }
-        }
     }
 }
 
---
+++ LibraryManagementSystem/Controllers/MembersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LibraryManagementSystem.Models;
+
+namespace LibraryManagementSystem.Controllers
+{
+	public class MembersController
+	{
+        //create methods here for CRUD operations for memebers
+
+
+        /// <summary>
+        /// Creating a private list of books to stimulate a data store
+        /// </summary>
+        ///
+        private List<MemberService> members = new List<MemberService>
+        {
+            new MemberService {memberId = 1, bookId = 1, name = "Stefanos", transactionId = 1},
+            new MemberService { memberId = 2, bookId = 2, name= "Mus", transactionId = 2},
+            new MemberService { memberId = 3, bookId = 3, name= "Stasia", transactionId = 3}
+        }; 
+
+        public void AddMember(MemberService member)
+		{
+            members.Add(member);
+		}
+	}
+}
+
