ede721891a26c3aaa0089102517f4fbf043cc76f
Stefanos Ugbit <stefanosu422@gmail.com>
2024-07-12 18:13:22 -0400

    added delete method

--- LibraryManagementSystem/Controllers/TransactionController.cs
+++ LibraryManagementSystem/Controllers/TransactionController.cs
@@ -1,40 +1,49 @@
-﻿using LibraryManagementSystem.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿    using LibraryManagementSystem.Models;
+    using Microsoft.AspNetCore.Mvc;
 
-namespace LibraryManagementSystem.Controllers
-{
-	public class TransactionController : Controller
-	{
-		//create methods here for CRUD operations for transactions
+    namespace LibraryManagementSystem.Controllers
+    {
+        public class TransactionController : Controller
+        {
+            //create methods here for CRUD operations for transactions
 
-		//using list to stimulate a data store
-		private List<Transaction> transactions = new List<Transaction>
-		{
-			new Transaction { transactionId = 1, bookId = 1, memberId = 1 },
-			new Transaction { transactionId = 2, bookId = 2, memberId = 2 },
-			new Transaction { transactionId = 3, bookId = 3, memberId = 3 },
-			new Transaction { transactionId = 4, bookId = 4 ,memberId = 3 }
-		};
+            //using list to stimulate a data store
+            private List<Transaction> transactions = new List<Transaction>
+            {
+                new Transaction { transactionId = 1, bookId = 1, memberId = 1 },
+                new Transaction { transactionId = 2, bookId = 2, memberId = 2 },
+                new Transaction { transactionId = 3, bookId = 3, memberId = 3 },
+                new Transaction { transactionId = 4, bookId = 4 ,memberId = 3 }
+            };
 
-		//add a transaction 
-        public void AddTransaction(Transaction transaction)
-		{
-            transactions.Add(transaction);
-		}
+            //add a transaction 
+            public void AddTransaction(Transaction transaction)
+            {
+                transactions.Add(transaction);
+            }
 
-		public List<Transaction> getAllTransactions()
-		{
-            return transactions;
-		}
+            public List<Transaction> getAllTransactions()
+            {
+                return transactions;
+            }
 
-		public void	updateTransaction(int transactionId, Transaction updateTransaction)
-		{
-			var transaction = transactions.Find(transaction => transactionId == transaction.transactionId);
-			if(transaction != null)
-			{
-				transaction.memberId = updateTransaction.memberId; 
-			}
-		}
-	}
-}
+            public void updateTransaction(int transactionId, Transaction updateTransaction)
+            {
+                var transaction = transactions.Find(transaction => transactionId == transaction.transactionId);
+                if (transaction != null)
+                {
+                    transaction.memberId = updateTransaction.memberId;
+                }
+            }
+            //remove a transaction
+            public void removeTransaction(int transactionId)
+            {
+                var transations = transactions.Find(transaction => transactionId == transaction.transactionId);
+                if(transactionId != null)
+                {
+                    removeTransaction(transactionId); 
+                }
+            }
+        }
+    }
 
